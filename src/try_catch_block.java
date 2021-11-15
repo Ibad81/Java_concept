a89643191db894550c3a5b5f1d6f8e666222eb4d
user <ibadbaig921@gmail.com>
2021-11-15 22:05:24 +0530

    Second commit

--- src/try_catch_block.java
+++ src/try_catch_block.java
@@ -1,14 +1,32 @@
 public class try_catch_block {
+//
+//    Example 1:
+//
+//    public static void main(String[] args) {
+//        try
+//        {
+//            int data=50/0; //may throw exception
+//        }
+//        //handling the exception
+//        catch(ArithmeticException e)
+//        {
+//            System.out.println(e);
+//        }
+//        System.out.println("rest of the code");
+//    }
+
+
     public static void main(String[] args) {
-        try
-        {
-            int data=50/0; //may throw exception
+        try {
+            int data = 50 / 0; //may throw exception
+            // if exception occurs, the remaining statement will not exceute
+            System.out.println("rest of the code");
         }
-        //handling the exception
-        catch(ArithmeticException e)
-        {
+        // handling the exception
+        catch (ArithmeticException e) {
             System.out.println(e);
         }
-        System.out.println("rest of the code");
     }
 }
+
+
