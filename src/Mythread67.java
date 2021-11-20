a0a09ec9df62096cee01f8ecaedd9f0ed572fc3a
user <ibadbaig921@gmail.com>
2021-11-20 10:35:29 +0530

    Multi threading

---
+++ src/Mythread67.java
@@ -0,0 +1,12 @@
+public class Mythread67 extends Thread{
+    public void run()
+    {
+        for (int i=0;i<10;i++){
+            System.out.println("Mythread "+1);
+        }
+    }
+}
+class DemoException{
+    
+
+}
