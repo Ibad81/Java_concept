cf6cfe5392d8b39f6f2a6c717833a9bfce3d6170
user <ibadbaig921@gmail.com>
2021-11-27 16:36:47 +0530

    Multi catch Block

--- src/Multi_catch_Block.java
+++ src/Multi_catch_Block.java
@@ -1,23 +1,42 @@
+//public class Multi_catch_Block {
+//    public static void main(String[] args) {
+//        try{
+////            int a[]=new int[5];
+////            System.out.println(a[10]);
+////            a[5]=30/0;
+//            String s=null;
+//            System.out.println(s.length());
+//        }catch (ArithmeticException e){
+//            System.out.println("Arthmetic Exception occurs");
+//        }
+//        catch(ArrayIndexOutOfBoundsException e){
+//            System.out.println("Array Index Out of Bounds Exception");
+//        }
+//        catch(Exception e){
+//            System.out.println("Parent Exception occurs");
+//        }
+//        System.out.println("Rest of the code");
+//
+//    }
+//}
+
 public class Multi_catch_Block {
     public static void main(String[] args) {
         try{
-//            int a[]=new int[5];
-//            System.out.println(a[10]);
-//            a[5]=30/0;
-            String s=null;
-            System.out.println(s.length());
-        }catch (ArithmeticException e){
-            System.out.println("Arthmetic Exception occurs");
+            int a[]=new int[5];
+            a[5]=30/0;
+        }catch(Exception e){
+            System.out.println("common task completed");
+        }
+        catch(ArithmeticException e){
+            System.out.println("task1 is completed");
         }
         catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Array Index Out of Bounds Exception");
+            System.out.println("task2 is completed");
         }
-        catch(Exception e){
-            System.out.println("Parent Exception occurs");
-        }
-        System.out.println("Rest of the code");
-
+        System.out.println("rest of the code");
     }
 }
 
 
+//Compile time error
