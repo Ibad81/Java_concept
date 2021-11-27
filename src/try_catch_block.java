dae38cfc65028d6cadff0db5837be3b1e8e47496
user <ibadbaig921@gmail.com>
2021-11-27 11:03:39 +0530

    Try catch block

--- src/try_catch_block.java
+++ src/try_catch_block.java
@@ -1,14 +1,34 @@
 
-public class try_catch_block{
+//public class try_catch_block{
+//    public static void main(String[] args) {
+//        try{
+//            System.out.println(50/0);
+//        }catch (Exception obj){
+//
+//            System.out.println(obj);
+//        }
+//    }
+//}
+
+
+
+import java.util.Scanner;
+
+public class try_catch_block {
     public static void main(String[] args) {
         try{
-            System.out.println(50/0);
-        }catch (Exception obj){
-
+            Scanner sc =new Scanner(System.in);
+            int a=sc.nextInt();
+        }catch(Exception obj){
             System.out.println(obj);
+        }
+       finally {
+            System.out.println("Final Block Execute");
+
         }
     }
 }
+
 
 
 
