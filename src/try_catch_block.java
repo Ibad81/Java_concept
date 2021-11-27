dcd14dcdba73c1ae9dbda1f78be829adc04c5934
user <ibadbaig921@gmail.com>
2021-11-27 11:31:33 +0530

    Try catch block

--- src/try_catch_block.java
+++ src/try_catch_block.java
@@ -44,24 +44,41 @@
 //        System.out.println("rest of code");
 //    }
 //}
+//
+//import java.util.Scanner;
+//
+//public class public class try_catch_block {
+//    public static void main(String[] args) {
+//        try{
+//            Scanner sc =new Scanner(System.in);
+//            int a=sc.nextInt();
+//        }catch(Exception obj){
+//            System.out.println(obj);
+//        }
+//       finally {
+//            System.out.println("Final Block Execute");
+//
+//        }
+//    }
+//}
 
-import java.util.Scanner;
 
-public class public class try_catch_block {
-    public static void main(String[] args) {
-        try{
-            Scanner sc =new Scanner(System.in);
-            int a=sc.nextInt();
-        }catch(Exception obj){
-            System.out.println(obj);
-        }
-       finally {
-            System.out.println("Final Block Execute");
 
-        }
-    }
+//public class try_catch_block{
+//    public static void main(String[] args) {
+//        try{
+//            int data =50/0;
+//        }catch( StringIndexOutOfBoundsException e){
+//            System.out.println(e);
+//        }
+//        System.out.println("rest of code");
+//    }
+//}
+
+
+public class try_catch_block{
+
 }
-
 
 
 
