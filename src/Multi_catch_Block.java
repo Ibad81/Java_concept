6c61b313c527845a975768ac2d729c6053b876e5
user <ibadbaig921@gmail.com>
2021-11-27 23:35:16 +0530

    TRY NESTED BLOCK

--- src/Multi_catch_Block.java
+++ src/Multi_catch_Block.java
@@ -20,23 +20,23 @@
 //    }
 //}
 
-public class Multi_catch_Block {
-    public static void main(String[] args) {
-        try{
-            int a[]=new int[5];
-            a[5]=30/0;
-        }catch(Exception e){
-            System.out.println("common task completed");
-        }
-        catch(ArithmeticException e){
-            System.out.println("task1 is completed");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("task2 is completed");
-        }
-        System.out.println("rest of the code");
-    }
-}
-
-
-//Compile time error
+//public class Multi_catch_Block {
+//    public static void main(String[] args) {
+//        try{
+//            int a[]=new int[5];
+//            a[5]=30/0;
+//        }catch(Exception e){
+//            System.out.println("common task completed");
+//        }
+//        catch(ArithmeticException e){
+//            System.out.println("task1 is completed");
+//        }
+//        catch(ArrayIndexOutOfBoundsException e){
+//            System.out.println("task2 is completed");
+//        }
+//        System.out.println("rest of the code");
+//    }
+//}
+//
+//
+////Compile time error
---
+++ src/Try_nested_example.java
@@ -0,0 +1,23 @@
+public class Try_nested_example {
+    public static void main(String[] args) {
+        try{
+            try{
+                System.out.println("going to divide by 0");
+                int b=39/0;
+            }catch(ArithmeticException e){
+                System.out.println(e);
+            }
+            try{
+                int a[]=new int[5];
+                a[5]=4;
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
+            System.out.println("other statement");
+        }catch(Exception e){
+            System.out.println("handled the exception (outer catch");
+        }
+        System.out.println("normal flow...");
+    }
+}
