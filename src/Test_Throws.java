97f2304cf5fcc540980251a63353a34645d336f6
user <ibadbaig921@gmail.com>
2021-11-28 11:57:26 +0530

    Use of Throws Keywords

--- src/Test_Throws.java
+++ src/Test_Throws.java
@@ -23,3 +23,17 @@
 //}
 
 
+import java.io.IOException;
+
+class M{
+    void method() throws IOException{
+        throw new IOException("device error ");
+    }
+}
+class Test_Throws{
+    public static void main(String[] args) throws IOException {
+        M m=new M();
+        m.method();
+        System.out.println("normal flow...");
+    }
+}
